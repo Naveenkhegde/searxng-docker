0b1d8457088b9ef8828fb2ec6cf8d267e5653596
rattfieldnz <emailme@robertattfield.com>
2022-07-24 02:02:06 +1200

    Initial SFTP setup script for searxng user.

---
+++ sftp/UNDO_searxng_sftp_user.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This script is intended for use on a cloud server 
+# (e.g. Amazon AWS, Vultr, DigitalOcean, AliBaba, etc).
+
+clear
+
+# Remove and purge SSH on server, if not already installed.
+# Below command also removes /etc/ssh/sshd_config
+sudo apt --purge remove openssh-server ssh -y
+
+# Remove created user for SFTP / SSH.
+sudo userdel searxng
+
+# Keep user home directory by default. 
+# Assign permissions to 'ubuntu' user.
+sudo chown root:root /home/searxng
+sudo chown ubuntu:root /home/searxng/*
